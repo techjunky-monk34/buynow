a867ad28f47caf7129e09bfa202a7438caba8df3
TJ Monk <techjunky.monk34@gmail.com>
2025-03-01 09:23:43 -0800

    t6

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -12,58 +11,25 @@ app.use((req, res, next) => {
     next();
 });
 
-
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
+
+// Import routes
+const contactRoutes = require('./contact_server');
+const aboutRoutes = require('./about_server');
+
+// Use the routes
+app.use(contactRoutes);
+app.use(aboutRoutes);
 
 // Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'templates', 'index.html'));
 });
 
-app.get('/about', (req, res) => {
-    const aboutPath = path.join(__dirname, 'templates', 'about.html');
-    console.log(`Serving about page from: ${aboutPath}`);
-    res.sendFile(aboutPath);
-
-
-
-});
-
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-=======
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Middleware for parsing JSON bodies
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Basic route to test the application
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
-});
-
-// Route to test POST requests
-app.post('/test', (req, res) => {
-    res.send('POST request received');
-});
-
-// Route to handle contact form submission
-app.post('/contact', (req, res) => {
-    const { name, email, phone, message } = req.body;
-    console.log(`Name: ${name}, Email: ${email}, Phone: ${phone}, Message: ${message}`);
-    res.send('Contact form submitted successfully');
-});
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
->>>>>>> 018ec50f08ee772e314dac9aa13f6d5d836cb188
